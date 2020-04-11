443a3d9456008b39e18d5a2baf748fb76cc85904
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2020-04-11 13:53:53 -0300

    Criadas as entidades principais

---
+++ AppWeb/Models/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projetoTelas
+{
+    [Table("Pessoas")]
+    public class Pessoa
+    {
+        [Key]
+        public int IdPessoa { get; set; }
+        [MaxLength(150)]
+        public string Nome { get; set; }
+        [MaxLength(15)]
+        public string Telefone { get; set; }
+        [MaxLength(8)]
+        public string PlacaVeiculo { get; set; }
+
+
+    }
+}
---
+++ AppWeb/Models/ServicoPrestado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projetoTelas
+{[Table("ServicosPrestados")]
+    public class ServicoPrestado
+    {
+        [Key]
+        public int IdServico { get; set; }
+        [MaxLength(2000)]
+        public string DescricaoServico { get; set; }
+        [Range(-999999999999999999.99, 999999999999999999.99)]
+        public decimal ValorTotal { get; set; }
+        [Range(-999999999999999999.99, 999999999999999999.99)]
+        public decimal ValorPago { get; set; }
+        [MaxLength(8)]
+        public string Pago { get; set; }
+        public DateTime DataServico { get; set; }
+        [ForeignKey("Pessoa")]
+        public int PessoaId { get; set; }
+        public virtual Pessoa Pessoa { get; set; }
+
+
+    }
+}
